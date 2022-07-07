07d863102fdf303618756fd116e1b36e9c6ae38a
Franco castañeda <castanedafranco994@gmail.com>
2022-07-06 21:19:08 -0300

    calculadora de cuotas

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
-let sumaNumPares = 0;
-for(i = 0; i <= 100; i++){
-    if(i % 2 == 0){
-        sumaNumPares += i;
+function cuotas() {
+    let valor;
+    let resultado;
+    let tresCuotas;
+    let seisCuotas;
+    let nueveCuotas;
+    let doceCuotas;
+
+    valor = document.getElementById("dinero").value;
+    valor = parseInt(valor);
+
+    tresCuotas = 3;
+    seisCuotas = 6;
+    nueveCuotas = 9;
+    doceCuotas = 12;
+
+    if(document.getElementById("tres").checked){
+        resultado = valor / tresCuotas;
+        alert("Las cuotas tienen un valor de : $" + resultado.toFixed(2))
+    }else if(document.getElementById("seis").checked){
+        resultado = valor / seisCuotas;
+        alert("Las cuotas tienen un valor de : $" + resultado.toFixed(2))
+    }else if(document.getElementById("nueve").checked){
+        resultado = valor / nueveCuotas;
+        alert("Las cuotas tienen un valor de : $" + resultado.toFixed(2))
+    }else if(document.getElementById("doce").checked){
+        resultado = valor / doceCuotas;
+        alert("Las cuotas tienen un valor de : $" + resultado.toFixed(2))
     }
 }
-console.log(sumaNumPares);
